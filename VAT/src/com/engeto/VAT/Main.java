f40e57ba05b75e124fd6303b640fd4f1738fbcdf
akcenurt <98740756+akcenurt@users.noreply.github.com>
2022-03-23 20:08:09 +0100

    read input added
    but SOUT needs to be replaced by sth that save input and use it to filter VAT

--- VAT/src/com/engeto/VAT/Main.java
+++ VAT/src/com/engeto/VAT/Main.java
@@ -10,6 +10,41 @@ public class Main {
     public static final String DELIMITER = "\t";
     public static final String OUTPUT_FILENAME = "vat-over-20.txt";
 
+    public class Support {
+
+        private static Scanner scanner = null;
+        private static Scanner getScanner() {
+            if (scanner == null) {
+                scanner = new Scanner(System.in);
+            }
+            return scanner;
+        }
+
+        public static int INVALID_INPUT = -1;
+
+        /**
+         * Read one integer. If given input cannot be converted to integer,
+         * prints error and return -1;
+         * @return Integer read or -1 if invalid input entered
+         */
+        public static int safeReadInt() {
+            int result = INVALID_INPUT;
+            String inputText = getScanner().nextLine();
+            try {
+                result = Integer.parseInt(inputText);
+            } catch (NumberFormatException ex) {
+                System.err.println("Zadal jsi text, který nelze převést na celé číslo: "+inputText);
+            }
+            return result;
+        }
+
+    }
+
+    public static void readOneIntFromInputAndPrintIt() {
+        int input = Support.safeReadInt();
+        System.out.println(input);
+    }
+
 
     public static void main(String[] args) {
 	StateList stateList = new StateList();
@@ -51,6 +86,8 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        readOneIntFromInputAndPrintIt();
 
         /* SLIBNÁ ČÁST TISKU BEZ ČÁRKY:
 
